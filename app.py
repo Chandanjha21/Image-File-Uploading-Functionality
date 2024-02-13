983e668507f1012aa01b0631e28047c24305777b
Chandan Kumar <ck8448470734@gmail.com>
2024-02-13 11:23:36 +0530

    solving some conflict

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask , render_template , request , url_for , redirect
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
